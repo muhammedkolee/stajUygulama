60bf22552cbab224c920ccaf972536a131a00a56
Muhammed Kole <muhammedkole18@gmail.com>
2025-05-30 12:47:05 +0300

    Template boşaltıldı.

--- App.js
+++ App.js
@@ -1,26 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Button, Image, Text, ScrollView, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Deniz Askilotam</Text>
-      <View>
-        <Text style={styles.daut}>
-          Hello, World!
-        </Text>
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
+  <View style = {styles.container}>
+    <Text style = {styles.textColor}>Merhaba</Text>
+  </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    // justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center"
   },
-
+  textColor: {
+    color: "green"
+  }
 });
